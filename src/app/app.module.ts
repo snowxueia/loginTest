b32e2661b1f4cf1cb5f8ef1d485185e79f0aa871
snowxueia <snowxueia2012@gmail.com>
2020-03-26 10:47:31 -0400

    my first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { environment } from 'src/environments/environment';
+import { AuthenticateService } from './services/authentication.service';
+//import { AngularFireModule } from '@angular/fire';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+ 
+import { AngularFireAuthModule } from '@angular/fire/auth';
+ 
+import * as firebase from 'firebase/app';
+ 
+firebase.initializeApp(environment.firebase);
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, 
+            IonicModule.forRoot(), 
+            AppRoutingModule,
+            ReactiveFormsModule
+           ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    AuthenticateService,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
